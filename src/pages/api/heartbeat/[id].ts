827d2a7a33b42e9d10062e66137f1a29d8f8536f
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-18 21:44:45 +0200

    Add a temp table view.

--- src/pages/api/heartbeat/[id].ts
+++ src/pages/api/heartbeat/[id].ts
@@ -1,23 +1,15 @@
 import withConnect from "../../../hoc/withConnect";
 import Heartbeat from '../../../models/heartbeat';
 
-const AliveHeartbeatRoute = async ({ body, method }, res) => {
-    const date = new Date();
-    date.setMinutes(date.getMinutes() - 10);
-
+const AliveHeartbeatRoute = async ({ body, method, query }, res) => {
     const heartbeats = await Heartbeat.find({
-        timestamp: {
-            $gte: date
-        }
+        id: query.id
     }, null, { sort: { '_id': -1 } });
 
     res.status(200).send({
         heartbeats: heartbeats.map(heartbeat => ({
             ...heartbeat.toObject()
-        })).reduce((arr, item) => {
-            const removed = arr.filter(i => i.id !== item.id);
-            return [...removed, item];
-        }, [])
+        }))
     });
 };
 
--- src/pages/id/[id].tsx
+++ src/pages/id/[id].tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import HearbeatList from '../../components/hearbeatlist';
+import Heartbeat from '../../components/heartbeat';
 
 const ServerPage = () => {
-    return 'Hello World';
+    const router = useRouter();
+    const { data } = useSWR(`/api/heartbeat/${router.query.id}`);
+
+    return (
+        <div>
+            <HearbeatList>
+                {data?.heartbeats.map(item => {
+                    return (
+                        <Heartbeat data={item} />
+                    );
+                })}
+            </HearbeatList>
+        </div>
+    );
 };
 
 export default ServerPage;
