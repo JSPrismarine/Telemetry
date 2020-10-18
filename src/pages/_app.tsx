79c2146395894a51116ba415056fac0858fce61a
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-18 23:49:34 +0200

    Fix responsiveness.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Head from 'next/head'
 import PageProvider from '../components/pageprovider';
 import '../scss/_app.scss';
 
 const App = ({ Component, pageProps }) => {
     return (
         <PageProvider>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
             <Component {...pageProps} />
         </PageProvider>
     );
