ee4b68deffaabd1cbaeaf9a85d9fa1d143220397
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-18 21:17:55 +0200

    Fix build.

--- src/components/pageprovider/index.tsx
+++ src/components/pageprovider/index.tsx
@@ -1,11 +1,7 @@
-import useSWR from 'swr';
-
-const PageProvider = () => {
-    const { data: alive } = useSWR('/api/heartbeat/alive');
-
+const PageProvider = ({ children }) => {
     return (
         <div>
-            <div>Alive servers: {alive?.heartbeats?.length}</div>
+            {children}
         </div>
     );
 };
