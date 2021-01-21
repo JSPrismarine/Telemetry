3d4351729a572672a6c9dbae286b75da6cf3fd62
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-01-21 23:28:38 +0000

    Proper list.

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,15 +1,20 @@
 import Link from 'next/link';
+import moment from 'moment';
 import useSWR from 'swr';
 
 const ErrorPage = () => {
     const { data } = useSWR(`/api/error`);
 
     return (
-        <div>
+        <ul>
             {data?.errors?.map?.((error) => (
-                <Link key={error.id} href={`/error/${error.id}`}></Link>
+                <li>
+                    <Link key={error.id} href={`/error/${error.id}`}>
+                        {moment(error.timestamp).format('LLLL')} - {error.name}: {error.message}
+                    </Link>
+                </li>
             ))}
-        </div>
+        </ul>
     );
 };
 
