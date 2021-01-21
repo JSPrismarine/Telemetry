429a03ca0407b7ba99e682984b781ab09a1ef603
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2021-01-21 22:52:48 +0000

    Print error somewhat nicely.

--- src/pages/error/[id].tsx
+++ src/pages/error/[id].tsx
@@ -1,13 +1,19 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
 const ServerPage = () => {
     const router = useRouter();
     const { data } = useSWR(`/api/error/${router.query.id}`);
+    const error = data.error;
 
     return (
         <div>
-            <code>{JSON.stringify(data, null, 4)}</code>
+            <h1>{error.id}</h1>
+            <h2>{error.name}</h2>
+            <SyntaxHighlighter style={dark}>{error.message}</SyntaxHighlighter>
+            <SyntaxHighlighter style={dark}>{error.stack}</SyntaxHighlighter>
         </div>
     );
 };
--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,13 +1,15 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
 const ErrorPage = () => {
-    const router = useRouter();
     const { data } = useSWR(`/api/error`);
 
     return (
         <div>
-            <code>{JSON.stringify(data, null, 4)}</code>
+            {data.errors.map((error) => (
+                <Link key={error.id} href={`/error/${error.id}`}></Link>
+            ))}
         </div>
     );
 };
