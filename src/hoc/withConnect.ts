ce3ae4b8accfaa559144fccf3a44f4b021b151d3
Filiph Siitam Sandström <filiph.sandstrom@filfatstudios.com>
2023-08-07 22:56:39 +0200

    fix: Remove unsupported argument.

--- src/hoc/withConnect.ts
+++ src/hoc/withConnect.ts
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const withConnect = (handler) => async (req, res) => {
     if (!mongoose.connection.readyState) {
         mongoose.connect(process.env.MONGODB as string, {
-            useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
             useCreateIndex: true
