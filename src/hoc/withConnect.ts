0478663cfab72913d8d5f5f773f54ec5e33b1843
Filiph Siitam Sandström <filiph.sandstrom@filfatstudios.com>
2023-08-08 03:14:12 +0200

    fix: Remove another unused argument.

--- src/hoc/withConnect.ts
+++ src/hoc/withConnect.ts
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 const withConnect = (handler) => async (req, res) => {
     if (!mongoose.connection.readyState) {
         mongoose.connect(process.env.MONGODB as string, {
-            useUnifiedTopology: true,
             useFindAndModify: false,
             useCreateIndex: true
         });
