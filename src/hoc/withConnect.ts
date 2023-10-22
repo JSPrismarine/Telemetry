2460c3735156d35d8f0e1b705ddd0be01af09361
Filiph Siitam Sandström <filiph@nordcom.io>
2023-10-22 16:54:34 +0200

    API: Remove legacy connection options.
    These have been removed from mongoose and were causing deployment errors (email spam).

--- src/hoc/withConnect.ts
+++ src/hoc/withConnect.ts
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 
 const withConnect = (handler) => async (req, res) => {
     if (!mongoose.connection.readyState) {
-        mongoose.connect(process.env.MONGODB as string, {
-            useFindAndModify: false,
-            useCreateIndex: true
-        });
+        mongoose.connect(process.env.MONGODB as string);
     }
 
     return handler(req, res);
